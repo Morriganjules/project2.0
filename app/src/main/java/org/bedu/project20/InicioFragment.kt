ad27977f86913957bf892fe6b8b42b4256d49fc1
Morriganjules <julietaeferreira@gmail.com>
2021-09-30 12:28:36 -0300

    sumando cosas

--- app/src/main/java/org/bedu/project20/InicioFragment.kt
+++ app/src/main/java/org/bedu/project20/InicioFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
 import androidx.core.view.isEmpty
 import androidx.navigation.NavController
 import androidx.navigation.NavDirections
@@ -16,9 +17,10 @@ import com.google.android.material.textfield.TextInputLayout
 class InicioFragment : Fragment() {
     private lateinit var btnRegister: MaterialButton
     private lateinit var btnLogin : MaterialButton
-    private lateinit var usuario : TextInputLayout
-    private lateinit var password : TextInputLayout
-
+    private lateinit var usuario : EditText
+    private lateinit var password : EditText
+    private lateinit var usuarioLayout: TextInputLayout
+    private lateinit var passwordLayout: TextInputLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,13 +35,22 @@ class InicioFragment : Fragment() {
         val view = inflater.inflate(R.layout.fragment_inicio, container, false)
         btnLogin = view.findViewById(R.id.btnLogin)
         btnRegister = view.findViewById(R.id.btnRegister)
-
-
+        usuario = view.findViewById(R.id.email)
+        password = view.findViewById(R.id.contraseña)
+        usuarioLayout = view.findViewById(R.id.emailLayout)
+        passwordLayout = view.findViewById(R.id.contraseñaLayout)
 
 
         btnLogin.setOnClickListener {
-            val action: NavDirections = InicioFragmentDirections.actionInicioFragmentToMainActivity()
-            findNavController().navigate(action)
+            if(validateUsuario(usuario)){
+                val action: NavDirections = InicioFragmentDirections.actionInicioFragmentToMainActivity()
+                findNavController().navigate(action)
+
+            }else{
+                usuarioLayout.isErrorEnabled()
+                usuarioLayout.setError("Ingrese un usuario valido")
+            }
+
         }
         btnRegister.setOnClickListener{
             val action: NavDirections = InicioFragmentDirections.actionInicioFragmentToRegisterFragment()
@@ -49,6 +60,12 @@ class InicioFragment : Fragment() {
 
         return view
     }
-
-
+    private fun validateUsuario(email: EditText):Boolean {
+    val emailInput = email.text.toString()
+    if (!emailInput.isEmpty() && emailInput.contains("@")) {
+        return true
+    }
+    return false
+  }
 }
+
--- app/src/main/java/org/bedu/project20/RegisterFragment.kt
+++ app/src/main/java/org/bedu/project20/RegisterFragment.kt
@@ -5,9 +5,22 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
+import com.google.android.material.button.MaterialButton
+import com.google.android.material.textfield.TextInputLayout
 
 
 class RegisterFragment : Fragment() {
+    private lateinit var nombre : EditText
+    private lateinit var correo : EditText
+    private lateinit var telefono : EditText
+    private lateinit var contraseña : EditText
+    private lateinit var btnRegistrar : MaterialButton
+    private lateinit var nombreLayout: TextInputLayout
+    private lateinit var correoLayout : TextInputLayout
+    private lateinit var telefonoLayout : TextInputLayout
+    private lateinit var contraseñaLayout :TextInputLayout
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,7 +32,21 @@ class RegisterFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_register, container, false)
+        val view = inflater.inflate(R.layout.fragment_register, container, false)
+        nombre =view.findViewById(R.id.textNombre)
+        correo = view.findViewById(R.id.textEmail)
+        telefono = view.findViewById(R.id.textTelefono)
+        contraseña = view.findViewById(R.id.textPassword)
+        btnRegistrar = view.findViewById(R.id.btnRegistrarse)
+        nombreLayout = view.findViewById(R.id.nombre)
+        correoLayout = view.findViewById(R.id.emailReg)
+        telefonoLayout = view.findViewById(R.id.telefono)
+        contraseñaLayout = view.findViewById(R.id.passwordReg)
+
+
+
+
+        return view
     }
 
 
