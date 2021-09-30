f64a51aaf6537edf74780d04fb812d0de71b441b
Morriganjules <julietaeferreira@gmail.com>
2021-09-30 11:55:10 -0300

    primeros arreglos

--- app/src/main/java/org/bedu/project20/InicioFragment.kt
+++ app/src/main/java/org/bedu/project20/InicioFragment.kt
@@ -5,9 +5,19 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.isEmpty
+import androidx.navigation.NavController
+import androidx.navigation.NavDirections
+import androidx.navigation.fragment.findNavController
+import com.google.android.material.button.MaterialButton
+import com.google.android.material.textfield.TextInputLayout
 
 
 class InicioFragment : Fragment() {
+    private lateinit var btnRegister: MaterialButton
+    private lateinit var btnLogin : MaterialButton
+    private lateinit var usuario : TextInputLayout
+    private lateinit var password : TextInputLayout
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,7 +30,24 @@ class InicioFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_inicio, container, false)
+        val view = inflater.inflate(R.layout.fragment_inicio, container, false)
+        btnLogin = view.findViewById(R.id.btnLogin)
+        btnRegister = view.findViewById(R.id.btnRegister)
+
+
+
+
+        btnLogin.setOnClickListener {
+            val action: NavDirections = InicioFragmentDirections.actionInicioFragmentToMainActivity()
+            findNavController().navigate(action)
+        }
+        btnRegister.setOnClickListener{
+            val action: NavDirections = InicioFragmentDirections.actionInicioFragmentToRegisterFragment()
+            findNavController().navigate(action)
+        }
+
+
+        return view
     }
 
 
